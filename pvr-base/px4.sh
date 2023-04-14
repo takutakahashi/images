c2d8ac66793d87e969701b32b80b5e53583dc78e
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2023-04-15 04:23:55 +0900

    make

--- pvr-base/px4.sh
+++ pvr-base/px4.sh
@@ -4,5 +4,5 @@ cd /px4_drv
 ls /lib/firmware/it930x-firmware.bin || mv fwtool/it930x-firmware.bin /lib/firmware
 cd driver
 apt update && apt install -y linux-headers-$(uname -r)
-make install
+make && make install
 modprobe px4_drv
